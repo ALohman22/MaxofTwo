175c204616315865786bcd6fbb83017b2c3ee69b
Andrew Lohman <andrewjlohman@gmail.com>
2023-01-24 11:19:01 -0700

    MAx of two commited

---
+++ maxoftwo.js
@@ -0,0 +1,10 @@
+let x = 1;
+let y = 2;
+
+if(x > y){
+    console.log(`Lhe largest value is ${x}`)
+} else if (y > x) {
+    console.log(`The Largest value is ${y}`)
+} else{
+    console.log(`The Values, ${y} and ${x}, are identical.`)
+}
